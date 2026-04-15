6edacbd8f34af9b0292fd3a78e66e7914dc9f609
HAMADNM <hamadmoideen111@gmail.com>
2026-04-15 23:31:25 +0530

    paetially completed

--- BuyNext/settings.py
+++ BuyNext/settings.py
@@ -55,7 +55,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-SITE_ID = 1
+SITE_ID = 2
 
 ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_LOGIN_METHODS = {"email", "username"}
